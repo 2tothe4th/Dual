487b5e87719e78a955caf5276e15980d2e39b951
bravery aung <onehundredtwentyeight@gmail.com>
2025-07-26 18:45:21 -0400

    Add Ord

--- src/Data/Dual.hs
+++ src/Data/Dual.hs
@@ -1,5 +1,4 @@
 module Data.Dual where
-import Linear
 import Control.Lens
 
 data Dual a = Dual { real :: a, nonreal :: a } deriving Eq
@@ -13,10 +12,17 @@ dualOne = Dual 1 0
 epsilon :: Num a => Dual a
 epsilon = Dual 0 1
 
-instance (Ord a, Num a, Show a) => Show (Dual a) where
-    show (Dual a 0) = show a
-    show (Dual 0 b) = show b ++ "ε"
-    show (Dual a b) = concat [show a, if b >= 0 then " + " else " - ", show (abs b), "ε"]
+instance (Ord a, Num a) => Ord (Dual a) where
+    compare a b
+        | difference == 0 = EQ
+        | c == 0 = compare d 0
+        | otherwise = compare c 0
+        where difference@(Dual c d) = a - b
+
+instance (Eq a, Num a, Show a) => Show (Dual a) where
+    showsPrec context (Dual a 0) = showsPrec context a
+    showsPrec context (Dual 0 b) = showParen (context > 7) $ showsPrec 7 b . showChar 'ε'
+    showsPrec context z = showParen (context > 6) $ showsPrec 6 (z { nonreal = 0 }) . showString " + " . showsPrec 7 (z { real = 0 })
 
 instance Functor Dual where
     fmap f (Dual a b) = Dual (f a) (f b)
@@ -30,24 +36,24 @@ instance Applicative Dual where
 instance Monad Dual where
     (Dual a b) >>= f = Dual (real $ f a) (nonreal $ f b)
 
-instance Num a => Num (Dual a) where
+instance (Ord a, Num a) => Num (Dual a) where
     (Dual a b) + (Dual c d) = Dual (a + c) (b + d)
     (Dual a b) * (Dual c d) = Dual (a * c) (a * d + b * c)
     negate = (negate <$>)
-    abs = error "The absolute value/magnitude of a dual number is currently undefined."
+    abs x = if x >= 0 then x else -x
     signum = (signum <$>)
     fromInteger = toDual . fromInteger
 
 conjugateDual :: Num a => Dual a -> Dual a
 conjugateDual (Dual a b) = Dual a (-b)
 
-instance (Eq a, Fractional a) => Fractional (Dual a) where 
+instance (Ord a, Fractional a) => Fractional (Dual a) where 
     Dual 0 b / Dual 0 d = Dual (b / d) 0
     Dual _ _ / Dual 0 _ = error "Division of the form (a + bε) / (dε) where a is non-zero is undefined."
     Dual a b / Dual c d = Dual (a / c) ((b * c - a * d) / (c * c))
     fromRational = toDual . fromRational
 
-instance (Eq a, Floating a) => Floating (Dual a) where
+instance (Ord a, Floating a) => Floating (Dual a) where
     -- TODO: A better definition of a ** b
     _ ** _ = error "Exponentiation of dual numbers is currently undefined."
     exp (Dual a b) = let scalar = exp a in Dual scalar (scalar * b)
@@ -58,7 +64,18 @@ instance (Eq a, Floating a) => Floating (Dual a) where
     cosh z = 0.5 * (exp z + exp (-z))
     tanh z = (exp z - exp (-z)) / (exp z + exp (-z))
 
-derive :: (Eq a, Fractional a) => (Dual a -> Dual a) -> (Dual a -> Dual a)
+rightSidedLimit :: (Ord a, Num a) => Dual a -> (Dual a -> Dual a) -> Dual a
+rightSidedLimit target function = toDual . real $ function (target + epsilon)
+
+leftSidedLimit :: (Ord a, Num a) => Dual a -> (Dual a -> Dual a) -> Dual a
+leftSidedLimit target function = toDual . real $ function (target - epsilon)
+
+limit :: (Ord a, Num a) => Dual a -> (Dual a -> Dual a) -> Maybe (Dual a)
+limit target function = if left == right then Just left else Nothing where
+    left = leftSidedLimit target function
+    right = rightSidedLimit target function
+
+derive :: (Ord a, Fractional a) => (Dual a -> Dual a) -> (Dual a -> Dual a)
 derive f x = (f (x + epsilon) - f x) / epsilon
 
 _real :: Lens (Dual a) (Dual a) a a
@@ -66,3 +83,6 @@ _real = lens real (\(Dual _ b) c -> Dual c b)
 
 _nonreal :: Lens (Dual a) (Dual a) a a
 _nonreal = lens nonreal (\(Dual a _) d -> Dual a d)
+
+_transpose :: Getter (Dual a) (Dual a)
+_transpose  = to (\(Dual a b) -> Dual b a)
