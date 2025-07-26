8e605ec4edfecc0ba298e74ce787913197d9c8f0
bravery aung <onehundredtwentyeight@gmail.com>
2025-07-26 15:36:03 -0400

    Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Overview
+Dual is a Haskell package, similar to Data.Complex, to work with [dual numbers](https://en.wikipedia.org/wiki/Dual_number). Dual numbers are constructed using the `Dual` constructor. Specifically, `Dual a b` = a + bε. At the moment, `Num`, `Fractional`, and `Floating` (not complete) instances have been defined for dual numbers.
+# Sources
+https://en.wikipedia.org/wiki/Dual_number
+https://math.stackexchange.com/questions/1914591/dual-number-ab-varepsilon-raised-to-a-dual-power-e-g-ab-varepsilon
+https://www.tiktok.com/@maths_visual/video/7530507874926136606
--- src/Data/Dual.hs
+++ src/Data/Dual.hs
@@ -1,10 +1,8 @@
 module Data.Dual where
-import Data.Functor
--- https://en.wikipedia.org/wiki/Dual_number
--- https://math.stackexchange.com/questions/1914591/dual-number-ab-varepsilon-raised-to-a-dual-power-e-g-ab-varepsilon
--- https://www.tiktok.com/@maths_visual/video/7530507874926136606
+import Linear
+import Control.Lens
 
-data Dual a = Dual { dualReal :: a, dualEpsilon :: a} deriving Eq
+data Dual a = Dual { real :: a, nonreal :: a } deriving Eq
 
 toDual :: Num a => a -> Dual a
 toDual x = Dual x 0
@@ -21,20 +19,22 @@ instance (Ord a, Num a, Show a) => Show (Dual a) where
     show (Dual a b) = concat [show a, if b >= 0 then " + " else " - ", show (abs b), "ε"]
 
 instance Functor Dual where
-    fmap function (Dual a b) = Dual (function a) (function b)
+    fmap f (Dual a b) = Dual (f a) (f b)
 
+-- Identical to Complex
 instance Applicative Dual where
     (Dual f g) <*> (Dual a b) = Dual (f a) (g b)
     pure x = Dual x x
 
+-- Also identical to Complex
 instance Monad Dual where
-    (Dual a _) >>= function = function a
+    (Dual a b) >>= f = Dual (real $ f a) (nonreal $ f b)
 
 instance Num a => Num (Dual a) where
     (Dual a b) + (Dual c d) = Dual (a + c) (b + d)
     (Dual a b) * (Dual c d) = Dual (a * c) (a * d + b * c)
     negate = (negate <$>)
-    abs (Dual a _) = Dual a 0 -- a + bε = ae^(bε/a), this defintion of abs is very subjective
+    abs = error "The absolute value/magnitude of a dual number is currently undefined."
     signum = (signum <$>)
     fromInteger = toDual . fromInteger
 
@@ -43,18 +43,26 @@ conjugateDual (Dual a b) = Dual a (-b)
 
 instance (Eq a, Fractional a) => Fractional (Dual a) where 
     Dual 0 b / Dual 0 d = Dual (b / d) 0
-    Dual _ _ / Dual 0 _ = error "Division of the form (a + bε)/(dε) where a is non-zero is undefined."
+    Dual _ _ / Dual 0 _ = error "Division of the form (a + bε) / (dε) where a is non-zero is undefined."
     Dual a b / Dual c d = Dual (a / c) ((b * c - a * d) / (c * c))
     fromRational = toDual . fromRational
 
 instance (Eq a, Floating a) => Floating (Dual a) where
+    -- TODO: A better definition of a ** b
+    _ ** _ = error "Exponentiation of dual numbers is currently undefined."
     exp (Dual a b) = let scalar = exp a in Dual scalar (scalar * b)
     log (Dual a b) = Dual (log a) (b / a)
     pi = toDual pi
     -- https://en.wikipedia.org/wiki/Hyperbolic_functions
-    sinh z = 0.5 * (exp z - exp (-z))
+    sinh z = (exp z - exp (-z)) / 2
     cosh z = 0.5 * (exp z + exp (-z))
     tanh z = (exp z - exp (-z)) / (exp z + exp (-z))
 
 derive :: (Eq a, Fractional a) => (Dual a -> Dual a) -> (Dual a -> Dual a)
 derive f x = (f (x + epsilon) - f x) / epsilon
+
+_real :: Lens (Dual a) (Dual a) a a
+_real = lens real (\(Dual _ b) c -> Dual c b)
+
+_nonreal :: Lens (Dual a) (Dual a) a a
+_nonreal = lens nonreal (\(Dual a _) d -> Dual a d)
