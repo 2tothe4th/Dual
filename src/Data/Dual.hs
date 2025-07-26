4fde0ab22caf639750979af54c3164de1e8bcf16
bravery aung <onehundredtwentyeight@gmail.com>
2025-07-26 01:46:36 -0400

    Add a few Floating functions

--- src/Data/Dual.hs
+++ src/Data/Dual.hs
@@ -1,7 +1,8 @@
-module Dual where
+module Data.Dual where
 import Data.Functor
---https://en.wikipedia.org/wiki/Dual_number
---Another video
+-- https://en.wikipedia.org/wiki/Dual_number
+-- https://math.stackexchange.com/questions/1914591/dual-number-ab-varepsilon-raised-to-a-dual-power-e-g-ab-varepsilon
+-- https://www.tiktok.com/@maths_visual/video/7530507874926136606
 
 data Dual a = Dual { dualReal :: a, dualEpsilon :: a} deriving Eq
 
@@ -22,13 +23,20 @@ instance (Ord a, Num a, Show a) => Show (Dual a) where
 instance Functor Dual where
     fmap function (Dual a b) = Dual (function a) (function b)
 
+instance Applicative Dual where
+    (Dual f g) <*> (Dual a b) = Dual (f a) (g b)
+    pure x = Dual x x
+
+instance Monad Dual where
+    (Dual a _) >>= function = function a
+
 instance Num a => Num (Dual a) where
     (Dual a b) + (Dual c d) = Dual (a + c) (b + d)
     (Dual a b) * (Dual c d) = Dual (a * c) (a * d + b * c)
-    negate z = negate <$> z
-    abs = undefined
-    signum z = signum <$> z
-    fromInteger x = Dual (fromInteger x) 0
+    negate = (negate <$>)
+    abs (Dual a _) = Dual a 0 -- a + bε = ae^(bε/a), this defintion of abs is very subjective
+    signum = (signum <$>)
+    fromInteger = toDual . fromInteger
 
 conjugateDual :: Num a => Dual a -> Dual a
 conjugateDual (Dual a b) = Dual a (-b)
@@ -37,7 +45,16 @@ instance (Eq a, Fractional a) => Fractional (Dual a) where
     Dual 0 b / Dual 0 d = Dual (b / d) 0
     Dual _ _ / Dual 0 _ = error "Division of the form (a + bε)/(dε) where a is non-zero is undefined."
     Dual a b / Dual c d = Dual (a / c) ((b * c - a * d) / (c * c))
-    fromRational = makeDual
+    fromRational = toDual . fromRational
+
+instance (Eq a, Floating a) => Floating (Dual a) where
+    exp (Dual a b) = let scalar = exp a in Dual scalar (scalar * b)
+    log (Dual a b) = Dual (log a) (b / a)
+    pi = toDual pi
+    -- https://en.wikipedia.org/wiki/Hyperbolic_functions
+    sinh z = 0.5 * (exp z - exp (-z))
+    cosh z = 0.5 * (exp z + exp (-z))
+    tanh z = (exp z - exp (-z)) / (exp z + exp (-z))
 
 derive :: (Eq a, Fractional a) => (Dual a -> Dual a) -> (Dual a -> Dual a)
 derive f x = (f (x + epsilon) - f x) / epsilon
