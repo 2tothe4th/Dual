176e4b586bfa52faea20c9fc51d05a87af8ff7ce
bravery aung <onehundredtwentyeight@gmail.com>
2025-07-28 19:35:19 -0400

    Add support for more trig functions

--- src/Data/Dual.hs
+++ src/Data/Dual.hs
@@ -1,5 +1,9 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Parenthesize unary negation" #-}
 module Data.Dual where
 import Control.Lens
+import Control.Monad.Trans.State
+import Data.Maybe
 
 -- | The main data type.
 data Dual a = Dual { real :: a, nonreal :: a } deriving Eq
@@ -21,7 +25,7 @@ toDual x = Dual x 0
 dualOne :: Num a => Dual a
 dualOne = Dual 1 0
 
--- | Epsilon (such that ε^2 = 0 but ε /= 0).
+-- | Epsilon (such that ε ^ 2 = 0 but ε /= 0).
 --
 -- @
 -- epsilon = Dual 0 1
@@ -38,8 +42,8 @@ instance (Ord a, Num a) => Ord (Dual a) where
 
 instance (Eq a, Num a, Show a) => Show (Dual a) where
     showsPrec context (Dual a 0) = showsPrec context a
-    showsPrec context (Dual 0 b) = showParen (context > 7) $ showsPrec 7 b . showChar 'ε'
-    showsPrec context z = showParen (context > 6) $ showsPrec 6 (z { nonreal = 0 }) . showString " + " . showsPrec 7 (z { real = 0 })
+    showsPrec context (Dual 0 b) = showParen (context > 7) $ showsPrec 7 b . showString " * " . showChar 'ε'
+    showsPrec context z = showParen (context > 6) $ showsPrec 6 (realFlatten z) . showString " + " . showsPrec 7 (nonrealFlatten z)
 
 instance Functor Dual where
     fmap f (Dual a b) = Dual (f a) (f b)
@@ -67,17 +71,31 @@ instance (Ord a, Fractional a) => Fractional (Dual a) where
     Dual a b / Dual c d = Dual (a / c) ((b * c - a * d) / (c * c))
     fromRational = toDual . fromRational
 
+convertFunctionToDual :: Num a => (a -> a) -> (a -> a) -> (Dual a -> Dual a)
+convertFunctionToDual f f' (Dual a b) = Dual (f a) (b * f' a)
+
 instance (Ord a, Floating a) => Floating (Dual a) where
     -- TODO: A better definition of a ** b
-    _ ** _ = error "Exponentiation of dual numbers is currently undefined."
-    exp (Dual a b) = let scalar = exp a in Dual scalar (scalar * b)
-    log (Dual a b) = Dual (log a) (b / a)
+    c@(Dual a _) ** d
+        | a == 0 = error "(**) is currently undefined for purely nonreal bases."
+        | otherwise = exp (log c * d)
+    exp = convertFunctionToDual exp exp
+    log = convertFunctionToDual log recip
     pi = toDual pi
+    sin = convertFunctionToDual sin cos
+    cos = convertFunctionToDual cos (negate . sin)
+    -- https://en.wikipedia.org/wiki/Trigonometric_functions#Derivatives_and_antiderivatives
+    tan = convertFunctionToDual tan ((\x -> x * x) . recip . tan)
     -- https://en.wikipedia.org/wiki/Hyperbolic_functions
     sinh z = (exp z - exp (-z)) / 2
     cosh z = 0.5 * (exp z + exp (-z))
     tanh z = (exp z - exp (-z)) / (exp z + exp (-z))
 
+-- https://en.wikipedia.org/wiki/Newton%27s_method
+newton :: (Ord a, Fractional a, Monad m) => (Dual a -> Dual a) -> StateT (Dual a) m (Dual a)
+newton f = do
+    modify \x -> x - (f x / fromJust (derive f x))
+    get
 
 -- | Conjugate.
 --
@@ -118,10 +136,10 @@ limit target function = if left == right then Just left else Nothing where
 -- | Takes the derivative of a function.
 --
 -- @
--- derive f x = (f (x + epsilon) - f x) / epsilon
+-- derive f = flip limit (\x -> (f (x + epsilon) - f x) / epsilon)
 -- @
-derive :: (Ord a, Fractional a) => (Dual a -> Dual a) -> (Dual a -> Dual a)
-derive f x = (f (x + epsilon) - f x) / epsilon
+derive :: (Ord a, Fractional a) => (Dual a -> Dual a) -> (Dual a -> Maybe (Dual a))
+derive f = flip limit (\x -> (f (x + epsilon) - f x) / epsilon)
 
 -- | Sets the dual part to zero.
 --
@@ -154,6 +172,9 @@ _real = lens real (\(Dual _ b) c -> Dual c b)
 -- | A lens for getting the nonreal component of a dual number.
 _nonreal :: Lens (Dual a) (Dual a) a a
 _nonreal = lens nonreal (\(Dual a _) d -> Dual a d)
+
+conjugated :: Num a => Getter (Dual a) (Dual a)
+conjugated = to conjugateDual
 
 -- | The lens version of 'realFlatten'
 realFlattened :: Num a => Getter (Dual a) (Dual a)
